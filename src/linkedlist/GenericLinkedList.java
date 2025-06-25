84a7b44e58b1f6135e2ba72599dd8ce1987deff1
Eliandro <eliandro.gomes42@gmail.com>
2025-06-25 16:34:07 -0300

    refactor: translate all class, method, and variable names from Portuguese to English

---
+++ src/linkedlist/GenericLinkedList.java
@@ -0,0 +1,81 @@
+package linkedlist;
+
+public class GenericLinkedList<T> {
+
+    private Node<T> head;
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Lúdica");
+        person.setAge(15);
+
+        GenericLinkedList<Object> list = new GenericLinkedList<>();
+        list.addFirst(3);
+        list.addLast(6);
+        list.addFirst(person);
+        list.addLast(9);
+        list.addLast("beautiful");
+        list.addFirst("heart");
+        list.addMiddle(10, 9);
+
+        list.print();
+    }
+
+    public void addFirst(T value) {
+        Node<T> newNode = new Node<>(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addMiddle(T value, T referenceValue) {
+        if (head == null) {
+            addFirst(value);
+            return;
+        }
+
+        Node<T> temp = head;
+        while (temp.next != null && !temp.next.value.equals(referenceValue)) {
+            temp = temp.next;
+        }
+
+        if (temp.next != null) {
+            Node<T> newNode = new Node<>(value);
+            newNode.next = temp.next;
+            temp.next = newNode;
+        } else {
+            System.out.println("Reference value not found in the list.");
+        }
+    }
+
+    public void addLast(T value) {
+        if (head == null) {
+            addFirst(value);
+            return;
+        }
+
+        Node<T> temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = new Node<>(value);
+    }
+
+    public void print() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
